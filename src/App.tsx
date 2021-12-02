ab95879dae392a0330f6141e69104747baad6621
SandeshVakale <sandyrv.vakale@gmail.com>
2021-12-02 17:22:31 +0100

    header ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import {Layout} from "antd";
+import Cryptocurrencies from "./screens/cryptocurrencies";
+import Markets from "./screens/markets";
+import HeaderBar from "./components/header/header";
+import Exchanges from "./screens/exchanges";
+import './App.less';
 
+const {Content} = Layout
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <Layout className="App">
+          <HeaderBar />
+          <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+              <Routes>
+                  <Route path="overview" element={<Counter />} />
+                  <Route path="cryptocurrencies" element={<Cryptocurrencies />} />
+                  <Route path="exchanges" element={<Exchanges />} />
+                  <Route path="markets" element={<Markets />} />
+              </Routes>
+          </Content>
+      </Layout>
   );
 }
 
---
+++ src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { useNavigate } from "react-router-dom";
+import { SearchOutlined, SettingFilled } from '@ant-design/icons';
+import './header.less'
+import {useViewport} from "../../hooks";
+const {Header} = Layout
+const HeaderBar = () => {
+    const navigate = useNavigate();
+    const {width} = useViewport()
+    return <Header className='header'>
+        <h1 className='title' >Coindata</h1>
+        <Menu theme="dark" mode="horizontal" className={width < 620 ? 'menu-short' : 'menu-long'} defaultSelectedKeys={['overview']}>
+            <Menu.Item key={'overview'} className={'item'} onClick={() => navigate('overview')}>Overview</Menu.Item>
+            <Menu.Item key={'cryptocurrencies'} className={'item'} onClick={() => navigate('cryptocurrencies')}>Cryptocurrencies</Menu.Item>
+            <Menu.Item key={'exchanges'} className={'item'} onClick={() => navigate('exchanges')}>Exchanges</Menu.Item>
+            <Menu.Item key={'markets'} className={'item'} onClick={() => navigate('markets')}>Markets</Menu.Item>
+        </Menu>
+        <div>
+        <SearchOutlined style={{color: 'white', fontSize: 24, width: 30, position: 'absolute', right: 40, top: 20}}/>
+        <SettingFilled style={{color: 'white', fontSize: 24, width: 30, position: 'absolute', right: 5, top: 20}} />
+        </div>
+    </Header>
+}
+
+export default HeaderBar;
